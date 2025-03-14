tutao/tutanota

Introduce key verification (squash)

packages/tutanota-crypto/lib/encryption/AsymmetricKeyPair.ts
@@ -1,5 +1,6 @@
-import { RsaEccKeyPair, RsaKeyPair, RsaPublicKey } from "./RsaKeyPair.js"
+import { RsaEccKeyPair, RsaEccPublicKey, RsaKeyPair, RsaPublicKey } from "./RsaKeyPair.js"
 import { PQKeyPairs, PQPublicKeys } from "./PQKeyPairs.js"
+import { Versioned } from "@tutao/tutanota-utils"
 
 export enum KeyPairType {
 	RSA,
@@ -13,7 +14,7 @@ export type AbstractKeyPair = {
 	keyPairType: KeyPairType
 }
 
-export type AsymmetricPublicKey = RsaPublicKey | PQPublicKeys
+export type AsymmetricPublicKey = RsaPublicKey | RsaEccPublicKey | PQPublicKeys
 
 export type AbstractPublicKey = {
 	keyPairType: KeyPairType
@@ -35,6 +36,26 @@ export function isPqPublicKey(publicKey: AbstractPublicKey): publicKey is PQPubl
 	return publicKey.keyPairType === KeyPairType.TUTA_CRYPT
 }
 
+export function isVersionedPqPublicKey(versionedPublicKey: Versioned<AsymmetricPublicKey>): versionedPublicKey is Versioned<PQPublicKeys> {
+	return isPqPublicKey(versionedPublicKey.object)
+}
+
 export function isRsaPublicKey(publicKey: AbstractPublicKey): publicKey is RsaPublicKey {
 	return publicKey.keyPairType === KeyPairType.RSA
+}
+
+export function isVersionedRsaPublicKey(versionedPublicKey: Versioned<AsymmetricPublicKey>): versionedPublicKey is Versioned<RsaPublicKey> {
+	return isRsaPublicKey(versionedPublicKey.object)
+}
+
+export function isRsaEccPublicKey(publicKey: AbstractPublicKey): publicKey is RsaEccPublicKey {
+	return publicKey.keyPairType === KeyPairType.RSA_AND_ECC
+}
+
+export function isVersionedRsaEccPublicKey(versionedPublicKey: Versioned<AsymmetricPublicKey>): versionedPublicKey is Versioned<RsaEccPublicKey> {
+	return isRsaEccPublicKey(versionedPublicKey.object)
+}
+
+export function isVersionedRsaOrRsaEccPublicKey(versionedPublicKey: Versioned<AsymmetricPublicKey>): versionedPublicKey is Versioned<RsaPublicKey> {
+	return isVersionedRsaPublicKey(versionedPublicKey) || isVersionedRsaEccPublicKey(versionedPublicKey)
 }

src/mail-app/workerUtils/worker/WorkerLocator.ts
@@ -86,6 +86,8 @@ import { MailOfflineCleaner } from "../offline/MailOfflineCleaner.js"
 import type { QueuedBatch } from "../../../common/api/worker/EventQueue.js"
 import { Credentials } from "../../../common/misc/credentials/Credentials.js"
 import { AsymmetricCryptoFacade } from "../../../common/api/worker/crypto/AsymmetricCryptoFacade.js"
+import { KeyVerificationFacade } from "../../../common/api/worker/facades/lazy/KeyVerificationFacade"
+import { PublicKeyConverter } from "../../../common/api/worker/crypto/PublicKeyConverter"
 import { KeyAuthenticationFacade } from "../../../common/api/worker/facades/KeyAuthenticationFacade.js"
 import { PublicKeyProvider } from "../../../common/api/worker/facades/PublicKeyProvider.js"
 import { EphemeralCacheStorage } from "../../../common/api/worker/rest/EphemeralCacheStorage.js"
@@ -116,6 +118,7 @@ export type WorkerLocatorType = {
 	keyLoader: KeyLoaderFacade
 	publicKeyProvider: PublicKeyProvider
 	keyRotation: KeyRotationFacade
+	publicKeyConverter: PublicKeyConverter
 
 	// login
 	user: UserFacade
@@ -142,6 +145,7 @@ export type WorkerLocatorType = {
 	booking: lazyAsync<BookingFacade>
 	share: lazyAsync<ShareFacade>
 	cacheManagement: lazyAsync<CacheManagementFacade>
+	keyVerification: lazyAsync<KeyVerificationFacade>
 
 	// misc & native
 	configFacade: lazyAsync<ConfigurationDatabase>
@@ -279,7 +283,14 @@ export async function initLocator(worker: WorkerImpl, browserData: BrowserData) 
 
 	locator.keyLoader = new KeyLoaderFacade(locator.keyCache, locator.user, locator.cachingEntityClient, locator.cacheManagement)
 
+	locator.publicKeyConverter = new PublicKeyConverter()
+
 	locator.publicKeyProvider = new PublicKeyProvider(locator.serviceExecutor)
+
+	locator.keyVerification = lazyMemoized(async () => {
+		const { KeyVerificationFacade } = await import("../../../common/api/worker/facades/lazy/KeyVerificationFacade.js")
+		return new KeyVerificationFacade(locator.serviceExecutor, locator.sqlCipherFacade, locator.publicKeyConverter)
+	})
 
 	locator.asymmetricCrypto = new AsymmetricCryptoFacade(
 		locator.rsa,
@@ -287,6 +298,8 @@ export async function initLocator(worker: WorkerImpl, browserData: BrowserData) 
 		locator.keyLoader,
 		locator.cryptoWrapper,
 		locator.serviceExecutor,
+		locator.keyVerification,
+		locator.publicKeyConverter,
 		locator.publicKeyProvider,
 	)
 
@@ -300,6 +313,8 @@ export async function initLocator(worker: WorkerImpl, browserData: BrowserData) 
 		cache,
 		locator.keyLoader,
 		locator.asymmetricCrypto,
+		locator.publicKeyConverter,
+		locator.keyVerification,
 		locator.publicKeyProvider,
 		lazyMemoized(() => locator.keyRotation),
 	)
@@ -344,6 +359,7 @@ export async function initLocator(worker: WorkerImpl, browserData: BrowserData) 
 		locator.share,
 		locator.groupManagement,
 		locator.asymmetricCrypto,
+		locator.publicKeyConverter,
 		keyAuthenticationFacade,
 		locator.publicKeyProvider,
 	)
@@ -442,6 +458,7 @@ export async function initLocator(worker: WorkerImpl, browserData: BrowserData) 
 			locator.keyLoader,
 			await locator.recoverCode(),
 			locator.asymmetricCrypto,
+			locator.publicKeyConverter,
 		)
 	})
 	const aesApp = new AesApp(new NativeCryptoFacadeSendDispatcher(worker), random)

syntax-syndicate/tutanota-privacy-platform

packages/tutanota-crypto/lib/encryption/AsymmetricKeyPair.ts
@@ -1,5 +1,6 @@
-import { RsaEccKeyPair, RsaKeyPair, RsaPublicKey } from "./RsaKeyPair.js"
+import { RsaEccKeyPair, RsaEccPublicKey, RsaKeyPair, RsaPublicKey } from "./RsaKeyPair.js"
 import { PQKeyPairs, PQPublicKeys } from "./PQKeyPairs.js"
+import { Versioned } from "@tutao/tutanota-utils"
 
 export enum KeyPairType {
 	RSA,
@@ -13,7 +14,7 @@ export type AbstractKeyPair = {
 	keyPairType: KeyPairType
 }
 
-export type AsymmetricPublicKey = RsaPublicKey | PQPublicKeys
+export type PublicKey = RsaPublicKey | RsaEccPublicKey | PQPublicKeys
 
 export type AbstractPublicKey = {
 	keyPairType: KeyPairType
@@ -35,6 +36,26 @@ export function isPqPublicKey(publicKey: AbstractPublicKey): publicKey is PQPubl
 	return publicKey.keyPairType === KeyPairType.TUTA_CRYPT
 }
 
+export function isVersionedPqPublicKey(versionedPublicKey: Versioned<PublicKey>): versionedPublicKey is Versioned<PQPublicKeys> {
+	return isPqPublicKey(versionedPublicKey.object)
+}
+
 export function isRsaPublicKey(publicKey: AbstractPublicKey): publicKey is RsaPublicKey {
 	return publicKey.keyPairType === KeyPairType.RSA
+}
+
+export function isVersionedRsaPublicKey(versionedPublicKey: Versioned<PublicKey>): versionedPublicKey is Versioned<RsaPublicKey> {
+	return isRsaPublicKey(versionedPublicKey.object)
+}
+
+export function isRsaEccPublicKey(publicKey: AbstractPublicKey): publicKey is RsaEccPublicKey {
+	return publicKey.keyPairType === KeyPairType.RSA_AND_ECC
+}
+
+export function isVersionedRsaEccPublicKey(versionedPublicKey: Versioned<PublicKey>): versionedPublicKey is Versioned<RsaEccPublicKey> {
+	return isRsaEccPublicKey(versionedPublicKey.object)
+}
+
+export function isVersionedRsaOrRsaEccPublicKey(versionedPublicKey: Versioned<PublicKey>): versionedPublicKey is Versioned<RsaPublicKey> {
+	return isVersionedRsaPublicKey(versionedPublicKey) || isVersionedRsaEccPublicKey(versionedPublicKey)
 }

src/mail-app/workerUtils/worker/WorkerLocator.ts
@@ -86,6 +86,7 @@ import { MailOfflineCleaner } from "../offline/MailOfflineCleaner.js"
 import type { QueuedBatch } from "../../../common/api/worker/EventQueue.js"
 import { Credentials } from "../../../common/misc/credentials/Credentials.js"
 import { AsymmetricCryptoFacade } from "../../../common/api/worker/crypto/AsymmetricCryptoFacade.js"
+import { KeyVerificationFacade } from "../../../common/api/worker/facades/lazy/KeyVerificationFacade"
 import { KeyAuthenticationFacade } from "../../../common/api/worker/facades/KeyAuthenticationFacade.js"
 import { PublicKeyProvider } from "../../../common/api/worker/facades/PublicKeyProvider.js"
 import { EphemeralCacheStorage } from "../../../common/api/worker/rest/EphemeralCacheStorage.js"
@@ -142,6 +143,7 @@ export type WorkerLocatorType = {
 	booking: lazyAsync<BookingFacade>
 	share: lazyAsync<ShareFacade>
 	cacheManagement: lazyAsync<CacheManagementFacade>
+	keyVerification: lazyAsync<KeyVerificationFacade>
 
 	// misc & native
 	configFacade: lazyAsync<ConfigurationDatabase>
@@ -277,12 +279,18 @@ export async function initLocator(worker: WorkerImpl, browserData: BrowserData) 
 
 	locator.publicKeyProvider = new PublicKeyProvider(locator.serviceExecutor)
 
+	locator.keyVerification = lazyMemoized(async () => {
+		const { KeyVerificationFacade } = await import("../../../common/api/worker/facades/lazy/KeyVerificationFacade.js")
+		return new KeyVerificationFacade(locator.sqlCipherFacade, locator.publicKeyProvider)
+	})
+
 	locator.asymmetricCrypto = new AsymmetricCryptoFacade(
 		locator.rsa,
 		locator.pqFacade,
 		locator.keyLoader,
 		locator.cryptoWrapper,
 		locator.serviceExecutor,
+		locator.keyVerification,
 		locator.publicKeyProvider,
 	)
 
@@ -296,6 +304,7 @@ export async function initLocator(worker: WorkerImpl, browserData: BrowserData) 
 		cache,
 		locator.keyLoader,
 		locator.asymmetricCrypto,
+		locator.keyVerification,
 		locator.publicKeyProvider,
 		lazyMemoized(() => locator.keyRotation),
 	)
@@ -438,6 +447,7 @@ export async function initLocator(worker: WorkerImpl, browserData: BrowserData) 
 			locator.keyLoader,
 			await locator.recoverCode(),
 			locator.asymmetricCrypto,
+			locator.publicKeyProvider,
 		)
 	})
 	const aesApp = new AesApp(new NativeCryptoFacadeSendDispatcher(worker), random)