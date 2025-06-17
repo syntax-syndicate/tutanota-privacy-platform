3dbfd1ac5e6badbceac7cb90316c0d3eca345470
and <and@tutao.de>
2025-06-17 10:44:50 +0200

    EventBanner and TimeView - Code cleanup and reorganization

--- src/common/gui/main-styles.ts
+++ src/common/gui/main-styles.ts
@@ -3165,6 +3165,12 @@ styles.registerStyle("main", () => {
 				transform: "translateX(100%)",
 			},
 		},
+		".time-indicator": {
+			position: "absolute",
+			background: theme.content_accent,
+			height: px(2),
+			width: "100%",
+		},
 		".time-indicator:before": {
 			content: "''",
 			display: "block",
